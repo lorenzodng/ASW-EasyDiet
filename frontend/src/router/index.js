68926c503c61413e59e3cec0b6361b3336ba1f04
Mara Pulighe <mara.pulighe@studio.unibo.it>
2025-12-17 21:28:12 +0100

    Componente home e creazione altri componenti e cartella pages

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from "vue-router"
 import Login from "../components/Login.vue"
 import Form from "../components/Form.vue"
 import Home from "../components/Home.vue"
+import ComponiDieta from "../components/ComponiDieta.vue"
 
 const routes = [
   {
@@ -20,6 +21,11 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: "/componi-dieta",
+    name: "ComponiDieta",
+    component: ComponiDieta
   }
 ]
 
