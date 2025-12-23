8fb62de19e1d11330258d27131bff34dcf8949c3
Mara Pulighe <mara.pulighe@studio.unibo.it>
2025-12-23 17:09:53 +0100

    infoModel diete

--- backend/src/diet/infoModel.js
+++ backend/src/diet/infoModel.js
@@ -1,164 +1,74 @@
-// struttura delle diete
-
 import mongoose from "mongoose";
+import mongoose from "mongoose";
+import { RecipeSchema } from "../recipes/infoModel.js";
+
 
 const DietSchema = new mongoose.Schema(
-    {
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "accounts",
-            required: true,
-            unique: true // una dieta attiva per utente
-        },
-
-        kcalUser: {
-            type: Number,
-            required: true
-        },
-
-        settimana: {
-            Lunedì: {
-                colazione: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                pranzo: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                merenda: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes",
-                    default: null
-                },
-                cena: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                }
-            },
-
-            Martedì: {
-                colazione: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                pranzo: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                merenda: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                cena: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                }
-            },
-
-            Mercoledì: {
-                colazione: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                pranzo: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                merenda: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                cena: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                }
-            },
-
-            Giovedì: {
-                colazione: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                pranzo: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                merenda: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                cena: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                }
-            },
-
-            Venerdì: {
-                colazione: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                pranzo: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                merenda: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                cena: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                }
-            },
-
-            Sabato: {
-                colazione: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                pranzo: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                merenda: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                cena: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                }
-            },
-
-            Domenica: {
-                colazione: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                pranzo: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                merenda: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                },
-                cena: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "recipes"
-                }
-            }
-        },
-
-        dataCreazione: {
-            type: Date,
-            default: Date.now
-        }
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "accounts",
+      required: true,
+      unique: true
     },
-    { versionKey: false }
+
+    kcalUser: {
+      type: Number,
+      required: true
+    },
+
+    settimana: {
+      Lunedì: {
+        colazione: RecipeSchema,
+        pranzo: RecipeSchema,
+        merenda: RecipeSchema,
+        cena: RecipeSchema
+      },
+      Martedì: {
+        colazione: RecipeSchema,
+        pranzo: RecipeSchema,
+        merenda: RecipeSchema,
+        cena: RecipeSchema
+      },
+      Mercoledì: {
+        colazione: RecipeSchema,
+        pranzo: RecipeSchema,
+        merenda: RecipeSchema,
+        cena: RecipeSchema
+      },
+      Giovedì: {
+        colazione: RecipeSchema,
+        pranzo: RecipeSchema,
+        merenda: RecipeSchema,
+        cena: RecipeSchema
+      },
+      Venerdì: {
+        colazione: RecipeSchema,
+        pranzo: RecipeSchema,
+        merenda: RecipeSchema,
+        cena: RecipeSchema
+      },
+      Sabato: {
+        colazione: RecipeSchema,
+        pranzo: RecipeSchema,
+        merenda: RecipeSchema,
+        cena: RecipeSchema
+      },
+      Domenica: {
+        colazione: RecipeSchema,
+        pranzo: RecipeSchema,
+        merenda: RecipeSchema,
+        cena: RecipeSchema
+      }
+    },
+
+    dataCreazione: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { versionKey: false }
 );
 
-const DietInfo = mongoose.model("diet", DietSchema);  //per collegarsi su mongoDB
+const DietInfo = mongoose.model("diet", DietSchema);
 export default DietInfo;
